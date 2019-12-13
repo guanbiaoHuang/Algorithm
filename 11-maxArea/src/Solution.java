a593eb4be6cb3bd2dd5c4f0771df564b3f13e516
jason <820245630@qq.com>
2019-12-13 10:28:47 +0800

    update
    Signed-off-by: jason <820245630@qq.com>

--- 11-maxArea/src/Solution.java
+++ 11-maxArea/src/Solution.java
@@ -9,7 +9,9 @@ public class Solution {
         System.out.println(new Solution().maxArea(new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7}));
     }
 
-    //暴力破解法
+    /**
+     * 暴力破解法
+     */
     private int maxArea(int[] height) {
         int max = 0;
         for (int i = 0; i < height.length; i++) {
@@ -20,7 +22,24 @@ public class Solution {
         return max;
     }
 
+    /**
+     * 双指针法
+     * 理解：两线段之间形成的区域总是会受到其中较短那条长度的限制。此外，两线段距离越远，得到的面积就越大。
+     * 所以两个指针，一个头一个尾，然后较短向较高的一端移动
+     */
     private int maxArea2(int[] height) {
-        return 0;
+        int start = 0, end = height.length - 1;
+        int maxarea = (end - start) * Math.min(height[start], height[end]);
+        while (start != end) {
+            //较短一端向高的一端移动
+            if (height[start] < height[end]) {
+                start++;
+            } else {
+                end--;
+            }
+            //获取当前最大面积
+            maxarea = Math.max(maxarea, (end - start) * Math.min(height[start], height[end]));
+        }
+        return maxarea;
     }
 }
