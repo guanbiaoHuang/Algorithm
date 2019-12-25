9bc8507ac548ae6e8be295e2f92c938a3bfaa5bd
jason <820245630@qq.com>
2019-12-25 17:17:20 +0800

    update
    Signed-off-by: jason <820245630@qq.com>

--- 15-threeSum/src/Solution.java
+++ 15-threeSum/src/Solution.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * LeetCode 15.三数之和
  *
@@ -6,26 +10,59 @@
 public class Solution {
 
     public static void main(String[] args) {
-        System.out.println(new Solution().romanToInt("MCMXCIV"));
+        System.out.println(new Solution().threeSum(new int[]{-1, 0, 1, 2, -1, -4}));
     }
 
     /**
-     * 参考整数转罗马数字
+     * 三数之和，化为双数之和，遍历数组，将数组中的数作为两数之和的答案的相反数，即完成转变
      */
-    private int romanToInt(String s) {
-        int ans = 0;
-        int index = 0;
-        int[] nums = new int[]{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-        String[] reps = new String[]{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-        while (index < s.length() - 1) {
-            for (int i = 0; i < 13; i++) {
-                if (s.startsWith(reps[i], index)) {
-                    ans += nums[i];
-                    index += reps[i].length();
+    private List<List<Integer>> threeSum(int[] nums) {
+
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if (null == nums || nums.length < 3) {
+            return result;
+        }
+        //先将数组排序，可实现双指针
+        Arrays.sort(nums);
+        int l, r;
+        //外层循环固定两数之和的答案
+        for (int i = 0; i < nums.length; i++) {
+            //去掉重复的的两数之和答案
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            //从左到右，如果当前已经大于0，那么就坑定没有指定三个数
+            if (nums[i] > 0) {
+                break;
+            }
+            r = nums.length - 1;
+            l = i + 1;
+            //内层循环，双指针
+            while (l < r) {
+                //三数之和大于0，右边指针往左移
+                if (nums[l] + nums[r] + nums[i] > 0) {
+                    r--;
+                    //三数之和小于0，右边指针往右移
+                } else if (nums[l] + nums[r] + nums[i] < 0) {
+                    l++;
+                } else {
+                    //相等时添加到链表中
+                    result.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                    //去除相同的答案
+                    while (l < r && nums[l] == nums[l + 1]) {
+                        l++;
+                    }
+                    while (l < r && nums[r] == nums[r - 1]) {
+                        r--;
+                    }
+                    //继续遍历剩余的数看是否右其他答案满足
+                    l++;
+                    r--;
                 }
             }
+
         }
-        return ans;
+        return result;
     }
 
 }
